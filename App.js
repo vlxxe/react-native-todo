65269e3389de7bbfd6d86bcecc67f8568c4dcf2f
vlxxe <tokzic01@gmail.com>
2020-01-17 23:14:11 +0300

    start refactoring with context

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React, { useState } from 'react'
-import { StyleSheet, View, Alert } from 'react-native'
 import * as Font from 'expo-font'
 import { AppLoading } from 'expo'
 
-import { NavBar } from './src/components/NavBar'
-import { MainScreen } from './src/screens/MainScreen'
-import { TodoScreen } from './src/screens/TodoScreen'
-import { THEME } from './src/theme'
+import { MainLayout } from './src/MainLayout'
+import { TodoState } from './src/context/todo/TodoState'
 
 async function loadApp() {
 	await Font.loadAsync({
@@ -17,8 +14,6 @@ async function loadApp() {
 
 export default function App() {
 	const [isReady, setIsReady] = useState(false)
-	const [todoId, setTodoId] = useState(null)
-	const [todos, setTodos] = useState([])
 
 	if (!isReady) {
 		return (
@@ -30,82 +25,9 @@ export default function App() {
 		)
 	}
 
-	const addTodo = title => {
-		setTodos(prev => [
-			...prev,
-			{
-				id: Date.now().toString(),
-				title,
-			},
-		])
-	}
-
-	const removeTodo = id => {
-		const todo = todos.find(t => t.id === id)
-		Alert.alert(
-			'Delete todo',
-			`Delete '${todo.title}' todo?`,
-			[
-				{
-					text: 'Cancel',
-					style: 'cancel',
-				},
-				{
-					text: 'Delete',
-					style: 'destructive',
-					onPress: () => {
-						setTodoId(null)
-						setTodos(prev => prev.filter(todo => todo.id !== id))
-					},
-				},
-			],
-			{ cancelable: false }
-		)
-	}
-
-	const updateTodo = (id, title) => {
-		setTodos(prev =>
-			prev.map(todo => {
-				if (todo.id === id) {
-					todo.title = title
-				}
-				return todo
-			})
-		)
-	}
-
-	let content = (
-		<MainScreen
-			todos={todos}
-			addTodo={addTodo}
-			removeTodo={removeTodo}
-			openTodo={setTodoId}
-		/>
-	)
-
-	if (todoId) {
-		const SelectedTodo = todos.find(todo => todo.id === todoId)
-		content = (
-			<TodoScreen
-				goBack={() => setTodoId(null)}
-				onRemove={removeTodo}
-				todo={SelectedTodo}
-				onSave={updateTodo}
-			/>
-		)
-	}
-
 	return (
-		<View>
-			<NavBar title="Todo App" />
-			<View style={styles.container}>{content}</View>
-		</View>
+		<TodoState>
+			<MainLayout />
+		</TodoState>
 	)
 }
-
-const styles = StyleSheet.create({
-	container: {
-		paddingHorizontal: THEME.PADDING_HORIZONTAL,
-		paddingVertical: 20,
-	},
-})
